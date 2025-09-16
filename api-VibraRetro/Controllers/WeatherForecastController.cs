83f547b20f75851968e85d03d412611a996a16cb
Ezequiel Figueroa <ezefiigueroa@gmail.com>
2025-09-16 09:25:50 -0300

    Creación de solución

---
+++ api-VibraRetro/Controllers/WeatherForecastController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace api_VibraRetro.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+   
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    
+}
